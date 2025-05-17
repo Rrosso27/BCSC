5c0ba143ceac427d61e8b05d75359a40c7c2f93d
Rrosso27 <125314912+Rrosso27@users.noreply.github.com>
2025-05-17 00:22:51 -0500

    Update Program.cs

--- BCSC/Program.cs
+++ BCSC/Program.cs
@@ -9,10 +9,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.ListenAnyIP(8080); // Escucha en el puerto 8080
-});
 
 builder.Services.AddTransient<IServicioEmail, ServicioEmail>();
 builder.Services.AddSingleton<EthereumService>();
@@ -23,6 +19,7 @@ var app = builder.Build();
 NodeManager.InitializeNodes(); // Asegurarse de que los nodos existan
 var synchronizedBlockchain = NodeManager.SynchronizeBlockchain();
 TokenSystem.SetBlockchain(synchronizedBlockchain);
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
